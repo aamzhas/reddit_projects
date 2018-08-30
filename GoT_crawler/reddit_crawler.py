f7f11a11fa79f5eda1ef3adac2587f3c47fec954
Aamir <hasanaamir215@gmail.com>
2018-08-29 22:18:08 -0500

    Cleaned up files and reformatted code accordingly

--- GoT_crawler/reddit_crawler.py
+++ GoT_crawler/reddit_crawler.py
@@ -1,26 +1,51 @@
+#!/usr/bin/env python
+
+"""
+Functions that help in extracting data from Game of Thrones Related subreddits.
+"""
+
 import csv
+
 import reddit_helpers
 
-# path where
-path = "./files/"
-comment_ids_filename = "comments_seen.txt"
-data_filename = "data.csv"
+__author__ = "Aamir Hasan"
+__version__ = "1.0"
+__email__ = "hasanaamir215@gmail.com"
 
-# crawl the given subreddit
+bot_name = 'GoT_Crawler'
+path = "../data/"
+data_filename = "got_crawler_data.csv"
+
+
 def crawl(reddit, subreddit):
+    """
+    Crawls through a subreddits comments and writes them into the csv file using the reddit object provided.
+
+    :param reddit: Reddit Instance of the bot
+    :param subreddit: The subreddit to be crawled
+    """
+
     data_file = open(path + data_filename, 'a+')
     csv_writer = csv.writer(data_file)
 
-    for comment in reddit.subreddit(subreddit).comments(limit=999999999999):
+    for comment in reddit.subreddit(subreddit).comments(limit=9999999):
         csv_writer.writerow([comment.id, comment.body, comment.score, comment.subreddit, comment.created])
 
     data_file.close()
 
+
 def gather_data():
-    reddit = reddit_helpers.authenticate()
+    """
+    Main function that gathers the data for the three popular game of thrones subreddits
+    """
+
+    reddit = reddit_helpers.authenticate(bot_name)
+
     subreddits = ['gameofthrones', 'asoiaf', 'freefolk']
+
     for subreddit in subreddits:
         crawl(reddit, subreddit)
+
 
 if __name__ == "__main__":
     gather_data()
--- reddit_helpers.py
+++ reddit_helpers.py
@@ -1,24 +1,62 @@
+#!/usr/bin/env python
+
+"""
+File contains helper funtions for all Reddit Projects
+"""
+
 import praw
 
-# authenticating and getting a reddit instance
-def authenticate():
+__author__ = "Aamir Hasan"
+__version__ = "1.0"
+__email__ = "hasanaamir215@gmail.com"
+
+
+def authenticate(bot_name):
+    """
+    Authenticates a Reddit bot and returns as reddit instance
+
+    :param bot_name: Name of the bot to be authenticated
+    :return: Reddit instance of bot
+    """
+
     print("Authenticating....")
 
-    reddit = praw.Reddit('GoT_Crawler', user_agent='Comment Extraction by /u/Bangoes')
+    try:
+        reddit = praw.Reddit(bot_name, user_agent=bot_name + " by u/Bangoes")
+    except Exception as e:
+        print("Authentication failed", str(e))
+        return False
 
     print("Authenticated Successfully")
     return reddit
 
-def read_from_file(path, filename):
-    file_reader = open(path + filename, "r")
-    seen_comments = file_reader.read().splitlines()
+
+def read_from_file(path):
+    """
+    Reading from a file
+
+    :param path: path to the file
+    :return: Contents of the file
+    """
+
+    file_reader = open(path, "r")
+    lines = file_reader.read().splitlines()
     file_reader.close()
 
-    return seen_comments
+    return lines
 
 
-def write_to_file(path, filename, new_comments):
-    file_writer = open(path + filename, "a+")
-    for id in new_comments:
-        file_writer.write(id + "\n")
+def write_to_file(path, lines):
+    """
+    Writes lines to a file
+
+    :param path: path to the file
+    :param lines: lines to be written
+    """
+
+    file_writer = open(path, "a+")
+
+    for line in lines:
+        file_writer.write(line + "\n")
+
     file_writer.close()
